7cf3f19399d87618299a7af3bbc11460cf5e835f
Martin Lozano <martin.lozano995@gmail.com>
2024-08-01 19:33:15 -0300

    add DrinkCard Component

--- src/views/IndexPage.tsx
+++ src/views/IndexPage.tsx
@@ -3,7 +3,6 @@ import { useAppStore } from "../stores/useAppStore"
 import DrinkCard from "../components/DrinkCard"
 
 
-
 export default function IndexPage() {
 
 const drinks = useAppStore((state) => state.drinks)
@@ -15,14 +14,16 @@ const hasDrinks = useMemo(() =>drinks.drinks.length , [drinks])
             <h1 className="text-6xl font-extrabold">Recetas</h1>
 
             {hasDrinks ? (
-                <>
+                <div
+                className="grid grid-cols-1 md:grid-cols-2 2xl:grid-cols-3 my-10"
+                >
                 {drinks.drinks.map((drink)=> (
 <DrinkCard
 key={drink.idDrink}
 drink={drink}
 />
                 ))}
-                </>
+                </div>
             ) :(
                 <p className="my-10 text-center text-2xl">
                     No hay resultados aun, utiliza el formulario
